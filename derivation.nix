0757c667f404325b081147d75d7b600a58a3f912
Tethys Svensson <freaken@freaken.dk>
2020-08-18 19:31:11 +0200

    Use nixpkgs/nixos-unstable. We need to update the cargo hash because of NixOS/nixpkgs#79975

--- derivation.nix
+++ derivation.nix
@@ -3,5 +3,5 @@ rustPlatform.buildRustPackage rec {
   pname = "date-stuff";
   version = "0.1.0";
   src = ./.;
-  cargoSha256 = "sha256-uNU4GvUhS2ZUGUfSQ69G+K3dr9rIFY1Ca2f2JhskIXA=";
+  cargoSha256 = "sha256-GUUz5rYTcHyMiLZ1U6cLTY/KTr3cXPXDPcBWl1oHmT4=";
 }
