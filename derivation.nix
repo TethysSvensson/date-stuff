0820e67aa55c150688fdf1214c5a8f3054d98175
Tethys Svensson <freaken@freaken.dk>
2021-04-13 17:15:42 +0200

    Bump lockfiles and cargo hash

--- derivation.nix
+++ derivation.nix
@@ -3,5 +3,5 @@ rustPlatform.buildRustPackage rec {
   pname = "date-stuff";
   version = "0.1.0";
   src = ./.;
-  cargoSha256 = "sha256-GUUz5rYTcHyMiLZ1U6cLTY/KTr3cXPXDPcBWl1oHmT4=";
+  cargoSha256 = "sha256-RpUdKqA5Wc6d7n01pLzeM1GEnGo5ik2Lvzm8nonM7Go=";
 }
