eeb186bdba3d314a0591a50f9190de92506f30f7
Tethys Svensson <freaken@freaken.dk>
2022-07-27 10:40:06 +0200

    Modernize date-stuff

--- derivation.nix
+++ derivation.nix
@@ -3,5 +3,5 @@ rustPlatform.buildRustPackage rec {
   pname = "date-stuff";
   version = "0.1.0";
   src = ./.;
-  cargoSha256 = "sha256-RpUdKqA5Wc6d7n01pLzeM1GEnGo5ik2Lvzm8nonM7Go=";
+  cargoSha256 = "sha256-Oq2GTXpNM3ztOJCxiZeY+q6E0L08GmW6QM6b4j/lBCE=";
 }
--- src/main.rs
+++ src/main.rs
@@ -1,182 +1,165 @@
-extern crate chrono;
-extern crate colored;
-#[macro_use]
-extern crate structopt;
-
-use chrono::naive::{IsoWeek, NaiveDate};
-use chrono::{Datelike, Local, Weekday};
-use colored::Colorize;
 use std::env;
-use structopt::StructOpt;
 
-trait WeekExt: Sized {
-    fn first_date(self) -> Option<NaiveDate>;
-    fn last_date(self) -> Option<NaiveDate>;
-    fn next_week(self) -> Option<Self>;
+use clap::Parser;
+use color_eyre::eyre::{eyre, ContextCompat, Result, WrapErr};
+use colored::Colorize;
+use time::{Date, Month, Weekday};
+
+#[derive(Copy, Clone)]
+pub struct IsoWeek {
+    year: i32,
+    week: u8,
 }
 
-impl WeekExt for IsoWeek {
-    fn first_date(self) -> Option<NaiveDate> {
-        NaiveDate::from_isoywd_opt(self.year(), self.week(), Weekday::Mon)
+impl IsoWeek {
+    pub fn new(date: Date) -> Self {
+        let (year, week, _day) = date.to_iso_week_date();
+        Self { year, week }
+    }
+    pub fn first_date(self) -> Result<Date> {
+        Date::from_iso_week_date(self.year, self.week, Weekday::Monday)
+            .wrap_err("Overflow when trying to find first date in week")
     }
 
-    fn last_date(self) -> Option<NaiveDate> {
-        NaiveDate::from_isoywd_opt(self.year(), self.week(), Weekday::Sun)
+    pub fn last_date(self) -> Result<Date> {
+        Date::from_iso_week_date(self.year, self.week, Weekday::Sunday)
+            .wrap_err("Overflow when trying to find last date in week")
     }
 
-    fn next_week(self) -> Option<IsoWeek> {
+    pub fn next_week(self) -> Result<IsoWeek> {
         let date = self.first_date()?;
-        Some(
-            date.checked_add_signed(chrono::Duration::weeks(1))?
-                .iso_week(),
-        )
+        Ok(IsoWeek::new(
+            date.checked_add(time::Duration::WEEK)
+                .wrap_err("Overflow when finding next week")?,
+        ))
     }
-}
 
-trait IsInMonthExt {
-    fn is_in_month(self, month: Month) -> bool;
-}
-
-impl IsInMonthExt for NaiveDate {
-    fn is_in_month(self, month: Month) -> bool {
-        self.year() == month.year && self.month() == month.month
-    }
-}
-
-impl IsInMonthExt for IsoWeek {
-    fn is_in_month(self, month: Month) -> bool {
-        self.first_date()
-            .map(|day| day.is_in_month(month))
-            .unwrap_or(false)
-            || self
-                .last_date()
-                .map(|day| day.is_in_month(month))
-                .unwrap_or(false)
+    pub fn weekdays(self) -> Result<[Date; 7]> {
+        let date = self.first_date()?;
+        let mut dates = [date; 7];
+        for i in 1..7 {
+            dates[i] = dates[i - 1]
+                .next_day()
+                .ok_or_else(|| eyre!("Overflow when finding the days of the week"))?;
+        }
+        Ok(dates)
     }
 }
 
 #[derive(Copy, Clone)]
-struct Month {
+struct CalendarMonth {
     year: i32,
-    month: u32,
+    month: Month,
 }
 
-impl Month {
-    fn first_date(self) -> Option<NaiveDate> {
-        NaiveDate::from_ymd_opt(self.year, self.month, 1)
+impl CalendarMonth {
+    fn first_date(self) -> Result<Date> {
+        Date::from_calendar_date(self.year, self.month, 1)
+            .wrap_err("Overflow when finding first date in month")
     }
 
-    fn first_week(self) -> Option<IsoWeek> {
-        Some(self.first_date()?.iso_week())
+    fn first_week(self) -> Result<IsoWeek> {
+        Ok(IsoWeek::new(self.first_date()?))
     }
 
-    fn pred(self) -> Month {
-        if self.month == 1 {
-            Month {
-                year: self.year.checked_sub(1).unwrap(),
-                month: 12,
-            }
+    fn contains_date(self, date: Date) -> bool {
+        self.year == date.year() && self.month == date.month()
+    }
+
+    fn contains_week(self, week: IsoWeek) -> Result<bool> {
+        Ok(self.contains_date(week.first_date()?) || self.contains_date(week.last_date()?))
+    }
+
+    fn previous(self) -> Result<CalendarMonth> {
+        if self.month == Month::January {
+            Ok(CalendarMonth {
+                year: self
+                    .year
+                    .checked_sub(1)
+                    .ok_or_else(|| eyre!("Overflow when finding the previous month"))?,
+                month: Month::December,
+            })
         } else {
-            Month {
+            Ok(CalendarMonth {
                 year: self.year,
-                month: self.month - 1,
-            }
+                month: self.month.previous(),
+            })
         }
     }
 
-    fn succ(self) -> Month {
-        if self.month == 12 {
-            Month {
-                year: self.year.checked_add(1).unwrap(),
-                month: 1,
-            }
+    fn next(self) -> Result<CalendarMonth> {
+        if self.month == Month::December {
+            Ok(CalendarMonth {
+                year: self
+                    .year
+                    .checked_add(1)
+                    .ok_or_else(|| eyre!("Overflow when finding the next month"))?,
+                month: Month::January,
+            })
         } else {
-            Month {
+            Ok(CalendarMonth {
                 year: self.year,
-                month: self.month + 1,
+                month: self.month.next(),
+            })
+        }
+    }
+
+    fn render_week(self, iso_week: IsoWeek, today: Date) -> Result<String> {
+        let mut output = String::with_capacity(7 * 3 + 2);
+        let week_str = format!("{:>2} ", iso_week.week).dimmed().to_string();
+        output.push_str(&week_str);
+
+        for date in iso_week.weekdays()? {
+            let mut curline = format!("{:>3}", date.day());
+
+            if !self.contains_date(date) {
+                curline = curline.dimmed().italic().to_string();
+            } else if date == today {
+                curline = curline.red().bold().to_string();
+            }
+            output.push_str(&curline);
+        }
+        Ok(output)
+    }
+
+    fn render(self, today: Date) -> Result<[String; 8]> {
+        let mut cur_week = self.first_week()?;
+        let mut output = [(); 8].map(|()| String::new());
+
+        output[0] = format!("   {:^21}", format!(" {} ", self.first_date()?.month()));
+        output[1] = "    Mo Tu We Th Fr Sa Su".to_string();
+
+        for o in output[2..].iter_mut() {
+            if self.contains_week(cur_week)? {
+                *o = self.render_week(cur_week, today)?;
+                cur_week = cur_week.next_week()?;
+            } else {
+                *o = "                        ".to_string();
             }
         }
+
+        Ok(output)
     }
 }
 
-fn weekdays(week: IsoWeek) -> impl Iterator<Item = NaiveDate> {
-    let mut date = week.first_date();
-    (0..7).filter_map(move |_| {
-        let result = date;
-        date = date.and_then(|date| date.succ_opt());
-        result
-    })
-}
-
-fn render_week(month: Month, week: IsoWeek, today: NaiveDate) -> String {
-    let mut output = String::with_capacity(7 * 3 + 2);
-    let week_str = format!("{:>2} ", week.week()).dimmed().to_string();
-    output.push_str(&week_str);
-
-    for date in weekdays(week) {
-        let mut curline = format!("{:>3}", date.day());
-
-        if !date.is_in_month(month) {
-            curline = curline.dimmed().italic().to_string();
-        } else if date == today {
-            curline = curline.red().bold().to_string();
-        }
-        output.push_str(&curline);
-    }
-    output
-}
-
-fn render_month(month: Month, today: NaiveDate) -> Option<[String; 8]> {
-    let mut cur_week = month.first_week()?;
-    let mut output = [
-        String::new(),
-        String::new(),
-        String::new(),
-        String::new(),
-        String::new(),
-        String::new(),
-        String::new(),
-        String::new(),
-    ];
-
-    output[0] = format!(
-        "   {:^21}",
-        format!(" {} ", month.first_date()?.format("%B"))
-    );
-
-    output[1] = "    Mo Tu We Th Fr Sa Su".to_string();
-    let mut pos = 2;
-    while cur_week.is_in_month(month) {
-        output[pos] = render_week(month, cur_week, today);
-        cur_week = cur_week.next_week()?;
-        pos += 1;
-    }
-
-    for o in &mut output[pos..] {
-        *o = "                        ".to_string();
-    }
-
-    Some(output)
-}
-
-#[derive(Debug, StructOpt)]
+#[derive(Debug, clap::Parser)]
 #[structopt(name = "date-stuff", about = "A better cal replacement")]
 struct Opt {
-    #[structopt(short = "b")]
+    #[clap(short = 'b')]
     before: Option<u32>,
-    #[structopt(short = "a")]
+    #[clap(short = 'a')]
     after: Option<u32>,
-    #[structopt(short = "c")]
+    #[clap(short = 'c')]
     context: Option<u32>,
     year: Option<i32>,
-    month: Option<u32>,
+    month: Option<u8>,
 }
 
 #[derive(Debug)]
 enum Command {
     RenderMonths {
         year: i32,
-        month: u32,
+        month: Month,
         before: u32,
         after: u32,
     },
@@ -185,21 +168,28 @@ enum Command {
     },
 }
 
-fn main() {
-    let mut opt = Opt::from_args();
-    let today = Local::today().naive_local();
+fn main() -> Result<()> {
+    color_eyre::install()?;
 
-    let get_env = |var, def| -> u32 {
-        match env::var(var).map(|s| s.parse()).unwrap_or(Ok(def)) {
-            Ok(v) => v,
-            Err(e) => panic!("Could not parse {}: {:?}", var, e),
+    let mut opt = Opt::parse();
+    let today = time::OffsetDateTime::now_local()?.date();
+
+    let get_env = |var, def| -> Result<u32> {
+        match env::var(var) {
+            Ok(s) => s
+                .parse()
+                .wrap_err_with(|| eyre!("Cannot parse environment variable {var} as a u32")),
+            Err(env::VarError::NotPresent) => Ok(def),
+            Err(e) => {
+                Err(e).wrap_err_with(|| eyre!("Cannot parse environment variable {var} as a u32"))
+            }
         }
     };
 
-    let before_noargs = get_env("DATE_BEFORE_NOARGS", 1);
-    let before_args = get_env("DATE_BEFORE_ARGS", 4);
-    let after_noargs = get_env("DATE_AFTER_NOARGS", 4);
-    let after_args = get_env("DATE_AFTER_ARGS", 4);
+    let before_noargs = get_env("DATE_BEFORE_NOARGS", 1)?;
+    let before_args = get_env("DATE_BEFORE_ARGS", 4)?;
+    let after_noargs = get_env("DATE_AFTER_NOARGS", 4)?;
+    let after_args = get_env("DATE_AFTER_ARGS", 4)?;
 
     opt.before = opt.before.or(opt.context);
     opt.after = opt.after.or(opt.context);
@@ -210,7 +200,9 @@ fn main() {
             let after = opt.after.unwrap_or(after_args);
             Command::RenderMonths {
                 year,
-                month,
+                month: month.try_into().wrap_err_with(|| {
+                    eyre!("The number {month} does not correspond to a valid month")
+                })?,
                 before,
                 after,
             }
@@ -235,7 +227,11 @@ fn main() {
 
     match cmd {
         Command::RenderYear { year } => {
-            months.extend((1..=12).map(|month| Month { year, month }));
+            let mut month = Month::January;
+            for _ in 0..12 {
+                months.push(CalendarMonth { year, month });
+                month = month.next();
+            }
         }
         Command::RenderMonths {
             year,
@@ -243,40 +239,40 @@ fn main() {
             before,
             after,
         } => {
-            let mut cur_month = Month { year, month };
+            let mut cur_month = CalendarMonth { year, month };
             for _ in 0..before {
-                cur_month = cur_month.pred();
+                cur_month = cur_month.previous()?;
             }
             for _ in 0..(before + 1 + after) {
                 months.push(cur_month);
-                cur_month = cur_month.succ();
+                cur_month = cur_month.next()?;
             }
         }
     }
 
     let months = months
         .into_iter()
-        .map(|m| render_month(m, today).unwrap())
-        .collect::<Vec<_>>();
+        .map(|m| m.render(today))
+        .collect::<Result<Vec<_>>>()?;
 
     for c in months.chunks(3) {
         match c {
             &[ref m0] => {
-                for l0 in m0.iter() {
+                for l0 in m0 {
                     if !l0.trim().is_empty() {
                         println!("{}", l0);
                     }
                 }
             }
             &[ref m0, ref m1] => {
-                for (l0, l1) in m0.iter().zip(m1.iter()) {
+                for (l0, l1) in m0.iter().zip(m1) {
                     if !(l0.trim().is_empty() && l1.trim().is_empty()) {
                         println!("{}    {}", l0, l1);
                     }
                 }
             }
             &[ref m0, ref m1, ref m2] => {
-                for ((l0, l1), l2) in m0.iter().zip(m1.iter()).zip(m2.iter()) {
+                for ((l0, l1), l2) in m0.iter().zip(m1).zip(m2) {
                     if !(l0.trim().is_empty() && l1.trim().is_empty() && l2.trim().is_empty()) {
                         println!("{}    {}    {}", l0, l1, l2);
                     }
@@ -285,4 +281,6 @@ fn main() {
             _ => unreachable!(),
         }
     }
+
+    Ok(())
 }
