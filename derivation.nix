579bb37c5cd562a4eb098ee6166d86e563852951
Tethys Svensson <freaken@freaken.dk>
2024-10-21 15:17:32 +0200

    Things

--- derivation.nix
+++ derivation.nix
@@ -1,7 +1,11 @@
-{ rustPlatform }:
-rustPlatform.buildRustPackage rec {
+{
+  rustPlatform,
+  nix-gitignore,
+}:
+
+rustPlatform.buildRustPackage {
   pname = "date-stuff";
   version = "0.1.0";
-  src = ./.;
+  src = nix-gitignore.gitignoreSource [ "*.nix" ] ./.;
   cargoSha256 = "sha256-Nsj9h6BXhUuxHyACEx0v45hd2qHGX8ZLW2AJWjR961I=";
 }
