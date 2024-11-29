5194f4d0f2d46939ab2c1d59a75b9ad7ea30576b
Tethys Svensson <freaken@freaken.dk>
2024-11-29 20:07:56 +0100

    Update nixpkgs to 24.11

--- derivation.nix
+++ derivation.nix
@@ -7,5 +7,5 @@ rustPlatform.buildRustPackage {
   pname = "date-stuff";
   version = "0.1.0";
   src = nix-gitignore.gitignoreSource [ "*.nix" ] ./.;
-  cargoSha256 = "sha256-4iRBYgCTLNwxMNm1COwPyuVckHkX4JAigYfckOvu0vg=";
+  cargoHash = "sha256-4iRBYgCTLNwxMNm1COwPyuVckHkX4JAigYfckOvu0vg=";
 }
