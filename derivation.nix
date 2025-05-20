762da781c0bdaa33da260c251f411a988feed899
Tethys Svensson <freaken@freaken.dk>
2025-05-20 09:46:52 +0200

    Bump hash too

--- derivation.nix
+++ derivation.nix
@@ -7,5 +7,5 @@ rustPlatform.buildRustPackage {
   pname = "date-stuff";
   version = "0.1.0";
   src = nix-gitignore.gitignoreSource [ "*.nix" ] ./.;
-  cargoHash = "sha256-Kt2QEXe7opfxKg0+nzDSGIvSJ32u7d6hDe+3Wug2Lnw=";
+  cargoHash = "sha256-omdIN+7tFLuZaJX/ExXicEQ/f+kQ1p5iMF1SOrNSFkg=";
 }
