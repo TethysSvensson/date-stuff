4190d3e7f10c98b3a9b9c674bdec82390a017c57
Tethys Svensson <freaken@freaken.dk>
2025-09-24 10:42:48 +0200

    Fix cargo hash

--- derivation.nix
+++ derivation.nix
@@ -7,5 +7,5 @@ rustPlatform.buildRustPackage {
   pname = "date-stuff";
   version = "0.1.0";
   src = nix-gitignore.gitignoreSource [ "*.nix" ] ./.;
-  cargoHash = "sha256-H5P9TtfPbajfvH8StCUewWPsTiO8bwPcJVUIq1ND+yE=";
+  cargoHash = "sha256-PMJB4lKTKukTt+TNLaRJHITQG4i8dAkWDNbV7SECERA=";
 }
